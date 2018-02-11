dbcd7d303d1b8fc45683c95d95ed0f6ae88567ba
Ray <ryteray@gmail.com>
2018-02-11 18:09:42 +0800

    Updated menu with buttons, functions empty atm

--- Classes/MenuManager.cpp
+++ Classes/MenuManager.cpp
@@ -34,17 +34,32 @@ void MenuManager::Init()
 	//set origin to middle
 	CURRENTSCENE->m_MenuLayer->setAnchorPoint(Vec2(0.5f, 0.5f));
 	//create a button with image
-	MenuItemImage* playButton = MenuItemImage::create("LightBlue_Front1.png", "Blue_Front1.png");
+	//MenuItemImage* playButton = MenuItemImage::create("LightBlue_Front1.png", "Blue_Front1.png");
 	//set origin to middle
-	playButton->setAnchorPoint(Vec2(0.5f, 0.5f));
+	//playButton->setAnchorPoint(Vec2(0.5f, 0.5f));
 
 	float x = SceneManager::getInstance().currScene_playingSize.width * 0.5f;
-	float y = SceneManager::getInstance().currScene_playingSize.height * 0.5f;
-	playButton->setPosition(Vec2(x,y)); //set to middle of screen
+	float y = SceneManager::getInstance().currScene_playingSize.height;
+	//playButton->setPosition(Vec2(x,y)); //set to middle of screen
 	//add it to scene menu layer
-	sceneMenuLayer->addChild(playButton);
+	//sceneMenuLayer->addChild(playButton);
 
-	playButton->setCallback(CC_CALLBACK_0(MenuManager::ExampleFunction,this));
+	//playButton->setCallback(CC_CALLBACK_0(MenuManager::ExampleFunction,this));
+
+
+	createButtonImageInScene("menu_img/playbtn.png", "menu_img/playbtn_selected.png", 
+		Vec2(0.5f, 0.5f), Vec2(x, y * 0.35f));
+
+	createButtonImageInScene("menu_img/controlsbtn.png", "menu_img/controlsbtn_selected.png",
+		Vec2(0.5f, 0.5f), Vec2(x, y* 0.28f));
+	createButtonImageInScene("menu_img/creditsbtn.png", "menu_img/creditsbtn_selected.png",
+		Vec2(0.5f, 0.5f), Vec2(x, y* 0.21f));
+	createButtonImageInScene("menu_img/optionbtn.png", "menu_img/optionbtn_selected.png",
+		Vec2(0.5f, 0.5f), Vec2(x, y* 0.14f));
+
+	createButtonImageInScene("menu_img/exitbtn.png", "menu_img/exitbtn_selected.png",
+		Vec2(0.5f, 0.5f), Vec2(x, y* 0.07f));
+
 }
 
 void MenuManager::Update(float dt)
@@ -74,3 +89,31 @@ MenuItemImage* MenuManager::createButtonImageInScene(const std::string& normalIm
 	sceneMenuLayer->addChild(temp);
 	return temp;
 }
+
+void MenuManager::PlayBtnStart()
+{
+	//Enter gameplay
+	CCLOG("Start game");
+}
+
+void MenuManager::ControlBtn_Pressed()
+{
+	CCLOG("View intructions");
+}
+
+void MenuManager::CreditsBtn_Pressed()
+{
+	CCLOG("Enter Credit page");
+}
+
+void MenuManager::OptionsBtn_Pressed()
+{
+	CCLOG("Options not done");
+}
+
+void MenuManager::ExitBtn_Pressed()
+{
+	CCLOG("Quit game");
+}
+
+
